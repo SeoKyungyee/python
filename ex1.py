823a1798c1bd3a0177f187c156c003e385945ebc
SeoKyungyee <167179719+SeoKyungyee@users.noreply.github.com>
2024-04-30 10:13:36 +0900

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,19 @@
+name = "박선영" ; avg = 88.3
+print("이름", name, "평균", avg, sep = "||", end = "\n\n")
+name = "장서경" ; avg =92.6
+print("이름", name, "평균", avg, sep = "*****", end = "\t")
+name = "박연우" ; avg =90.3
+print("이름", name, "평균", avg)
+print("출력을 종료합니다!")
+
+print("2개의 숫자를 입력하세요.")
+num1 = input()
+num2 = input()
+num3 = num1 + num2
+print(num3)
+
+print("2개의 숫자를 입력하세요.")
+num1 = int(input())
+num2 = int(input())
+num3 = num1 + num2
+print(num3)
