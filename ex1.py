608e9b2efbc54ea8f34c092fa367a607ad471c14
SeoKyungyee <167179719+SeoKyungyee@users.noreply.github.com>
2024-05-16 10:03:01 +0900

    Add files via upload

--- ex1.py
+++ ex1.py
@@ -1,30 +1,9 @@
-list_1 = [1, 2, 3, 4, 5, 1, 3]
-list_2 = []
-print(list_1)
-print(list_1)
+def sum_int(a, b):
+    return a+b
 
-print(len(list_1))
+num1 = 10
+num2 = 20
 
-list_1[3] = 9999
-print(list_1)
+result = sum_int(num1, num2)
+print(f'{num1} + {num2} = {result}')
 
-list_1.append(100)
-print(list_1)
-
-list_1 = [1, 2, 3, 9999, 5, 1, 3, 100]
-list_2 = []
-print(list_1)
-
-list_1.remove(9999)
-print(list_1)
-
-list_1.insert(0.777)
-print(list_1)
-
-list_2 = list_1.copy()
-print(list_2)
-
-list = [1, 2, 3, 5, 1, 3]
-
-for num in list :
-    print(num)
