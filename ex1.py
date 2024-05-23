5b9f53360e97b995cd30f2104940b82abb46aa92
SeoKyungyee <167179719+SeoKyungyee@users.noreply.github.com>
2024-05-23 10:28:27 +0900

    Add files via upload

--- ex1.py
+++ ex1.py
@@ -1,9 +1,15 @@
-def sum_int(a, b):
-    return a+b
+import pandas as pd
 
-num1 = 10
-num2 = 20
+list1 = list(
+    [
+        ["한빛", "남자", "20", "180"],
+        ["한결", "남자", "21", "177"],
+        ["한라", "여자", "20", "160"],
+    ]
+)
 
-result = sum_int(num1, num2)
-print(f'{num1} + {num2} = {result}')
+col_names = ["이름", "성별", "나이", "키"]
+df = pd.DataFrame(list1, columns=col_names)
+print(df)
 
+df.to_csv("file.csv", header=True, index=False)
