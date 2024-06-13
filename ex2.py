6c8ca2e7d0619cbff6763e41040a9ac40382c0fb
SeoKyungyee <167179719+SeoKyungyee@users.noreply.github.com>
2024-06-13 10:23:12 +0900

    Add files via upload

--- ex2.py
+++ ex2.py
@@ -1,22 +1,30 @@
 import pandas as pd
-import mariadb
+import matplotlib.pyplot as plt
+import seaborn as sns 
 
-conn = mariadb.connect(
-    user="user018"
-    password="!ai123",
-    host="edu.ithows.com",
-    port=53306,
-    database="edudb"
-)
-cursor = conn.cursor()
+def addtext(x,y):
+    for i in range(len(x)):
+        plt.text(i,y[i]+0.5,y[i], ha = 'center')
+        
+hat = pd.read_csv('ch4-1.csv') # hat 변수에 데이터셋 입력
+print(hat.head(), end="\n\n") # 위에서부터 5개 데이터 확인
 
-cursor.execute('SELECT * FROM city')
-results = cursor.fetchall()
-for row in results:
-    print(row)
-    
-df = pd.DataFrame(results)
-print(df.head(10), end='\n\n')
+#바차트 그리기
+plt.figure(figsize=(15, 10))
+plt.bar(hat['hatchery'], hat['chick'], color = ('red','orange','yellow','green','blue','navy','purple'))
 
-conn.close()
-    
+plt.title('hatchery statistics')
+plt.xlabel('hatchery')
+plt.ylabel('chick count')
+
+addtext(hat['hatchery'], hat['chick'])
+plt.show()
+
+# 파이차트 그리기
+pct = hat['chick']/hat['chick'].sum()
+col7 = sns.color_palette('Pastel2', 7)
+
+plt.figure(figsize=(10, 10))
+plt.pie(pct, labels = hat['hatchery'], autopct='%.1f%%', colors=col7, counterclock=False)
+
+plt.show()
